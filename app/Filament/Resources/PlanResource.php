9692b61368ca1580bad9d6cf98604fe2f51ab95f
kingn <yasseralhassan942@gmail.com>
2025-03-10 11:15:06 +0300

    feat: Add Plan Management Resources and Policies
    - Introduced `PlanResource` in `app/Filament/Resources` to manage subscription plans with Filament.
    - Added `CreatePlan`, `EditPlan`, and `ListPlans` pages under `PlanResource\Pages` for CRUD operations.
    - Created `Plan` model in `app/Models` with fillable attributes: name, description, price, duration, and status.
    - Implemented `PlanPolicy` in `app/Policies` to handle authorization for plan-related actions.
    - Added migration `2025_03_10_074925_create_plans_table.php` to create `plans` table with necessary fields.
    - Included `QuestionPolicy` and `TestPolicy` for managing permissions on questions and tests.

---
+++ app/Filament/Resources/PlanResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PlanResource\Pages;
+use App\Filament\Resources\PlanResource\RelationManagers;
+use App\Models\Plan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+class PlanResource extends Resource
+{
+    protected static ?string $model = Plan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+    protected static ?string $navigationGroup = 'Subscriptions Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$')
+                    ->minValue(0),
+
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->minValue(0),
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'active' => 'Active',
+                        'passive' => 'Passive',
+                    ])
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('description')
+                ->limit(50)
+                ,
+                TextColumn::make('price')
+                    ->numeric(decimalPlaces: 2)
+                    ->prefix('$ '),
+                TextColumn::make('duration')
+                    ->numeric()
+                    ->suffix(' days'),
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'active' => 'success',
+                        'passive' => 'danger',
+                    }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPlans::route('/'),
+            'create' => Pages\CreatePlan::route('/create'),
+            'edit' => Pages\EditPlan::route('/{record}/edit'),
+        ];
+    }
+}
