0ed66633a2021ca1da40b5f6fbcb7e0c6a979f4a
kingn <yasseralhassan942@gmail.com>
2025-03-15 14:29:52 +0300

    Add Subscription Expiration Command
    - Implemented a new console command `subscriptions:expire` to automatically expire subscriptions where the `end_date` has passed.
    - Updated the `Subscription` model to include necessary imports and maintain code structure.
    - Scheduled the `subscriptions:expire` command to run every minute using Laravel's scheduler in `routes/console.php`.
    - Ensured that only subscriptions with `ACTIVE` or `CANCELED` status are checked for expiration and updated to `EXPIRED` status.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+Schedule::call(
+    function () {
+        Artisan::call('subscriptions:expire');
+    }
+)->everyMinute();
